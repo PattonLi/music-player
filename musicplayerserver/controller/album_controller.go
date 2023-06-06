f281053926bdeb483c8d70ad9d7a803272fe0939
Kim Yang <1564321591@qq.com>
2023-06-06 11:06:47 +0800

    0

--- musicplayerserver/controller/album_controller.go
+++ musicplayerserver/controller/album_controller.go
@@ -37,3 +37,30 @@ func (ac *AlbumController) GetAlbumByIdHandler(c *gin.Context) (model.AlbumInfo,
 	album, err := ac.albumService.GetAlbumById(Id)
 	return album, err
 }
+
+// 获取特定名称专辑信息
+func (ac *AlbumController) AlbumInfoHandler(c *gin.Context) ([]gin.H, error) {
+	name := c.Query("name")
+	albumlist, err := ac.albumService.AlbumInfo(name)
+	albums := make([]gin.H, 0)
+	for _, album := range albumlist {
+		albuminfo := gin.H{
+			"albumId":  album.AlbumID,
+			"size":     album.Size,
+			"name":     album.Name,
+			"artist":   album.Artist,
+			"ArtistID": album.Artist_ID,
+			"profile":  album.Profile,
+		}
+		albums = append(albums, albuminfo)
+	}
+	return albums, err
+}
+
+// 获取特定页所有专辑信息
+func (ac *AlbumController) AllAlbumInfoHandler(c *gin.Context) ([]model.AlbumInfo, int64) {
+	page, _ := strconv.Atoi(c.Query("currentPage"))
+	pagesize, _ := strconv.Atoi(c.Query("pageSize"))
+	albumlist, totalPage := ac.albumService.AllAlbumInfo(page, pagesize)
+	return albumlist, totalPage
+}
