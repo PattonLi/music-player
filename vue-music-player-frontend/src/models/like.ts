bf42d3fef8c15e551159e1f44b2084efdd265638
lpt <373595331@qq.com>
2023-06-06 21:16:57 +0800

    完成用户前端api

--- vue-music-player-frontend/src/models/like.ts
+++ vue-music-player-frontend/src/models/like.ts
@@ -6,3 +6,11 @@ export interface Like {
   artistId: number
   type: number //喜欢类型(1：歌曲 2：歌手 3：专辑)
 }
+
+export interface LikeForm {
+  userId: number
+  songId: number
+  albumId: number
+  artistId: number
+  type: number //喜欢类型(1：歌曲 2：歌手 3：专辑)
+}
