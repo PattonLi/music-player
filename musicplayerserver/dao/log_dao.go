95f6f6d7025444efbdb80161670953f8ee460d18
Kmbls <1923713826@qq.com>
2023-07-05 21:58:22 +0800

    完成了管理端“评论管理”和“日志管理”

--- musicplayerserver/dao/log_dao.go
+++ musicplayerserver/dao/log_dao.go
@@ -11,3 +11,10 @@ func (l *LogDao) AddPlayLog(log model.LogInfo) error {
 	result := DB.Create(&log)
 	return result.Error
 }
+
+//根据type获得日志
+func (l *LogDao) GetLogByType(logtype int) ([]model.LogInfo,error) {
+	var loglist []model.LogInfo
+	err := DB.Where("type = ?", logtype).Find(&loglist).Error
+	return loglist, err
+}
